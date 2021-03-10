68035f50c6be22e323c00a5f1ae4d81407d7c51d
Mert Buran <mert.buran@datadoghq.com>
2021-03-11 00:53:55 +0100

    second commit

---
+++ SecondFramework/SecondFramework.h
@@ -0,0 +1,18 @@
+//
+//  SecondFramework.h
+//  SecondFramework
+//
+//  Created by Mert Buran on 11/03/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SecondFramework.
+FOUNDATION_EXPORT double SecondFrameworkVersionNumber;
+
+//! Project version string for SecondFramework.
+FOUNDATION_EXPORT const unsigned char SecondFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SecondFramework/PublicHeader.h>
+
+
---
+++ SecondFramework/Source.swift
@@ -0,0 +1,13 @@
+//
+//  Source.swift
+//  SecondFramework
+//
+//  Created by Mert Buran on 11/03/2021.
+//
+
+import Foundation
+import MyFramework
+
+public class Info {
+    public static let name = MyFramework.Info.name + " Secondary"
+}
